2ad267efb5802dee6bd6583fda993216f0235071
thiagohfagundes <eng.thiagofagundes@gmail.com>
2025-09-01 17:23:31 -0300

    Inserida a allowed host para deploy no railway

--- flows/settings.py
+++ flows/settings.py
@@ -36,7 +36,9 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.railway.app'
+]
 
 
 # Application definition
